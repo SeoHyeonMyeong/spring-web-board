5ad48047a6c017f299d5ae67c48f0625cfe17f15
SeoHyeonMyeong <gusaud2199@naver.com>
2022-04-30 19:07:20 +0900

    First Commit: Hello World [jdk 17]

---
+++ src/main/java/com/example/springwebboard/SpringWebBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebBoardApplication.class, args);
+	}
+
+}
