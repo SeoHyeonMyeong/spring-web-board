14eaa8c632799ac784176cc41f077a5953f93da6
SeoHyeonMyeong <gusaud2199@naver.com>
2022-05-04 23:04:37 +0900

    Add: Article-Api-Controller 새로운 api 패키지를 생성해서 article api controller 클래스를 생성 article-api-controller에 CRUD 기능을 추가 Article Entity에 patch 메서드 추가 기본 api-controller 클래스를 추가

---
+++ src/main/java/com/example/springwebboard/api/ArticleApiController.java
@@ -0,0 +1,67 @@
+package com.example.springwebboard.api;
+
+import com.example.springwebboard.dto.ArticleForm;
+import com.example.springwebboard.entity.Article;
+import com.example.springwebboard.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class ArticleApiController {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    // GET List
+    @GetMapping("/api/articles")
+    public List<Article> index() {
+        return articleRepository.findAll();
+    }
+
+    // GET One
+    @GetMapping("/api/articles/{id}")
+    public Article detail(@PathVariable Long id) {
+        return articleRepository.findById(id).orElseThrow();
+    }
+
+    // POST
+    @PostMapping("/api/articles")
+    public Article create(@RequestBody ArticleForm dto) {
+        Article article = dto.toEntity();
+        return articleRepository.save(article);
+    }
+
+    // PATCH
+    @PatchMapping("/api/articles/{id}")
+    public ResponseEntity<Article> update(@RequestBody ArticleForm dto,
+                          @PathVariable Long id) {
+        Article article = dto.toEntity();
+        Article target = articleRepository.findById(id).orElse(null);
+        if (target == null || id != target.getId()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+            }
+        target.patch(article);
+        Article updated = articleRepository.save(target);
+        return ResponseEntity.status(HttpStatus.OK).body(updated);
+
+    }
+
+    // DELETE
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Article> delete(@PathVariable Long id){
+        Article target = articleRepository.findById(id).orElse(null);
+        if (target == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        articleRepository.delete(target);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+
+}
+
