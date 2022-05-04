06c48e0417a21824997b852efe22f4180256d850
SeoHyeonMyeong <gusaud2199@naver.com>
2022-05-04 17:21:16 +0900

    add: 기본 덤프 데이터 4개 DB에 저장하기

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO article (title, content) VALUES ('aaaaa', '1111');
+INSERT INTO article (title, content) VALUES ('bbbbb', '2222');
+INSERT INTO article (title, content) VALUES ('ccccc', '3333');
+INSERT INTO article (title, content) VALUES ('ddddd', '4444');
